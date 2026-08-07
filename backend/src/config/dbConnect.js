6f09bbbf632ad8ad42385f19be77cf6879b41fa4
mehedi-hasan02 <mehedisani02@gmail.com>
2026-08-08 03:07:13 +0600

    update

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,18 +1,20 @@
-import dotenv from "dotenv";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fvwg0tw.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`;
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q9ntuh2.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`;
-
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(uri);
-    console.log(`MongoDB Connected: ${process.env.DB_NAME}`);
+    const uri = `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(
+      process.env.DB_PASS,
+    )}@cluster0.q9ntuh2.mongodb.net/${process.env.DB_NAME}?appName=Cluster0`;
+
+    const connection = await mongoose.connect(uri);
+
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB connection failed:", error);
+    throw error;
   }
 };
 
